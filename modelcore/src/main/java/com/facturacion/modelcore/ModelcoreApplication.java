aeec7eee3fdad44d5e4dbcb457ba8657de533654
unknown <jssorianoh@gmail.com>
2022-07-23 19:40:15 -0500

    agrega modelo

---
+++ modelcore/src/main/java/com/facturacion/modelcore/ModelcoreApplication.java
@@ -0,0 +1,13 @@
+package com.facturacion.modelcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelcoreApplication.class, args);
+	}
+
+}
---
+++ modelcore/src/test/java/com/facturacion/modelcore/ModelcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facturacion.modelcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
